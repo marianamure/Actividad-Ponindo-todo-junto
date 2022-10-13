d3d41960a05c637bc9d22b464397f4ef0fae62cd
Mariana Muñoz Restrepo <103978734+marianamure@users.noreply.github.com>
2022-10-13 11:43:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PersonCard from "./componentes/PersonCard/PersonCard";
 
 
 /* 
-Inicializar proyecto: npm sart
+Inicializar proyecto: npm start
 Detener proyecto: Ctrl +C
 */
 
@@ -13,9 +13,13 @@ class App extends React.Component {
   render() {
     return(
       <div className="container">
+        <br/>
         <PersonCard firstName="Alejandra" lastName="Villa" age={30} hairColor="Grey"/>
+        <br/>
         <PersonCard firstName="Ines" lastName="Caro" age={40} hairColor="Black"/>
+        <br/>
         <PersonCard firstName="Alan" lastName="David" age={50} hairColor="Brunette"/>
+        <br/>
         <PersonCard firstName="Daniel" lastName="Macias" age={50} hairColor="Black"/>
       </div>
     );
--- src/componentes/PersonCard/PersonCard.js
+++ src/componentes/PersonCard/PersonCard.js
@@ -1,31 +1,16 @@
-import React from "react";
+import React, {useState} from 'react';
 
-class PersonCard extends React.Component {
+const PersonCard = ({firstName, lastName, age, hairColor}) => {
 
-    constructor(props){
-        super (props);
-        this.state = {
-            age: this.props.age
-        }
-
-    }
-
-    birthday = () => {
-        this.setState({age:this.state.age+1})
-    }
-
-    render(){
-
-        const {firstName, lastName, age, hairColor} = this.props;
-
-        return(
+    return (
+        <div className = "col-5">
             <div className="card">
                 <h4>{firstName} {lastName}</h4>
-                <p><b>Age:</b>{this.state.age}</p>
+                <p><b>Age:</b>{age}</p>
                 <p><b>Hair Color:</b>{hairColor}</p>
-                <button className="btn btn-warning" onClick={this.birthday}> Birthday Button {firstName}</button>
             </div>
-        )
+        </div>
+    )
     }
-}
+
 export default PersonCard;
